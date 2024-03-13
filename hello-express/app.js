1426d8927214974afedaefe30a543de851fd2003
beetrbgus <devbeetrb0214@gmail.com>
2024-03-13 13:55:12 +0900

    feat - comment 라우터 추가

--- hello-express/app.js
+++ hello-express/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var commentRouter = require('./routes/comments') // 댓글 router 추가
 var sequelize = require('./models').sequelize; // ./models는 ./models/index.js와 동일. index.js파일은 require시 생략 가능
 
 var app = express();
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/comments', commentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
