dd870ee09af02b955fc58822a079cf5340e9ebd6
beetrbgus <devbeetrb0214@gmail.com>
2024-03-14 10:09:23 +0900

    feat - 댓글 조회, 등록 추가

--- hello-express/routes/comments.js
+++ hello-express/routes/comments.js
@@ -2,8 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:id', async (req, res, next) => {
+  try {
+    let comments = await Comment.findAll({
+      include : {
+        model : User,
+        where : { id: req.params.id },
+      },
+    });
+    res.json(comments);
+
+  } catch(error) {
+    console.error(error);
+    next(error);
+  }
 });
 
+router.post('/', async (req, res, next) => {
+  try {
+    let comment = await Comment.create({
+      commenter : req.body.id,
+      comment : req.body.comment,
+    });
+    res.status(201).json(comment);
+
+  } catch (error) {
+    console.error(error);
+    next(error);  
+  }
+});
 module.exports = router;
