ea7ebf820ad49422bfeb24b367f3731dc57c241a
beetrbgus <devbeetrb0214@gmail.com>
2024-03-14 10:09:03 +0900

    feat - 유저 등록, 조회 추가

--- hello-express/routes/users.js
+++ hello-express/routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
+var User = require('../models').User;
+
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch(error) {
+    next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    let user  = User.create({
+      name : req.body.name,
+      age : req.body.age,
+      married : req.body.married,
+    });
+
+    console.log(user);
+    res.status(201).json(user);
+  } catch(error) {
+    console.log(error);
+    next(error);
+  }
 });
 
 module.exports = router;
